a8d33007b6b3a05a3b14d0525502801d19918850
oanaBiancaSchiopu <80052076+oanaBiancaSchiopu@users.noreply.github.com>
2023-02-17 11:46:25 +0200

    fix-import (#27)
    Co-authored-by: Oana Schiopu <oana-schiopu@activeeon.com>

--- sal-service/src/main/java/org/ow2/proactive/sal/service/service/application/PASchedulerGateway.java
+++ sal-service/src/main/java/org/ow2/proactive/sal/service/service/application/PASchedulerGateway.java
@@ -38,7 +38,7 @@ import javax.security.auth.login.LoginException;
 import org.ow2.proactive.db.SortOrder;
 import org.ow2.proactive.db.SortParameter;
 import org.ow2.proactive.sal.service.util.SchedulerConnectionHelper;
-import org.ow2.proactive.scheduler.common.JobFilterCriteria;
+import org.ow2.proactive.scheduler.common.JobFilterCriteriaBuilder;
 import org.ow2.proactive.scheduler.common.JobSortParameter;
 import org.ow2.proactive.scheduler.common.Page;
 import org.ow2.proactive.scheduler.common.exception.*;
