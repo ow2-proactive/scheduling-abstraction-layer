cb287bb0eb1dfaff3acdcb17bf8edca25cbd419b
Mohamed Khalil LABIDI <47785603+mklkun@users.noreply.github.com>
2023-04-28 00:33:20 +0200

    Add dynamic private ip env var setting inside component tasks (#41)
    * Add dynamic private ip env var setting inside component tasks
    
    * Delete code smell
    
    * Apply spotless

--- sal-service/src/main/java/org/ow2/proactive/sal/service/service/TaskBuilder.java
+++ sal-service/src/main/java/org/ow2/proactive/sal/service/service/TaskBuilder.java
@@ -108,10 +108,10 @@ public class TaskBuilder {
     }
 
     private List<ScriptTask> createAppTasks(Task task, String taskNameSuffix, String taskToken, Job job) {
-        switch (task.getType()) {
-            case "commands":
+        switch (Objects.requireNonNull(Installation.InstallationType.fromValue(task.getType()))) {
+            case COMMANDS:
                 return createCommandsTask(task, taskNameSuffix, taskToken, job);
-            case "docker":
+            case DOCKER:
                 return createDockerTask(task, taskNameSuffix, taskToken, job);
             default:
                 return new LinkedList<>();
@@ -408,7 +408,7 @@ public class TaskBuilder {
             // Creating infra deployment tasks
             String token = task.getTaskId() + deployment.getNumber();
             String suffix = "_" + deployment.getNumber();
-            scriptTasks.add(createScalingChildsaveTask(task, suffix, token, job));
+            scriptTasks.add(createScalingChildSaveTask(task, suffix, token, job));
         });
         task.setDeploymentFirstSubmittedTaskName(scriptTasks.get(0)
                                                             .getName()
@@ -421,7 +421,7 @@ public class TaskBuilder {
         return scriptTasks;
     }
 
-    private ScriptTask createScalingChildsaveTask(Task task, String suffix, String token, Job job) {
+    private ScriptTask createScalingChildSaveTask(Task task, String suffix, String token, Job job) {
         ScriptTask scriptTaskUpdate = null;
 
         Map<String, TaskVariable> taskVariablesMap = new HashMap<>();
@@ -433,7 +433,8 @@ public class TaskBuilder {
                                                  .get(0)
                                                  .getRequestedName()));
 
-        if (!Strings.isNullOrEmpty(task.getInstallation().getUpdateCmd())) {
+        if (Installation.InstallationType.COMMANDS.getValue().equals(task.getType()) &&
+            !Strings.isNullOrEmpty(task.getInstallation().getUpdateCmd())) {
             scriptTaskUpdate = PAFactory.createBashScriptTask(task.getName() + "_update" +
                                                               suffix,
                                                               Utils.getContentWithFileName(EXPORT_ENV_VAR_SCRIPT) +
@@ -609,7 +610,7 @@ public class TaskBuilder {
                                                               "echo \"No ports to open and not parent tasks. Nothing to be prepared in VM.\"");
         }
 
-        if (task.getType().equals("commands")) {
+        if (Installation.InstallationType.COMMANDS.getValue().equals(task.getType())) {
             if (task.getInstallation()
                     .getOperatingSystemType()
                     .getOperatingSystemFamily()
--- sal-service/src/main/resources/export_env_var_script.sh
+++ sal-service/src/main/resources/export_env_var_script.sh
@@ -3,15 +3,25 @@ if [ -z "$variables_requestedPortName" ]; then
     echo "[Env_var] No requested ports for this task. Nothing to be set."
 else
     REQUESTED_PORT_NAME="PUBLIC_$variables_requestedPortName"
+    PRIVATE_REQUESTED_PORT_NAME="PRIVATE_$variables_requestedPortName"
+
     if [[ ! -z $variables_requestedPortName ]]; then
-        REQ="variables_$variables_requestedPortName"
+        REQ="variables_${variables_requestedPortName}"
         REQUESTED_PORT_VALUE=${!REQ}
+
+        REQ_PRV="variables_${variables_requestedPortName}_prv"
+        PRV_REQUESTED_PORT_VALUE=${!REQ_PRV}
 
         if [[ -z ${!REQUESTED_PORT_NAME} ]]; then
             echo "[Env_var] Variable $REQUESTED_PORT_NAME does not exist. Exporting ..."
             export $REQUESTED_PORT_NAME=$REQUESTED_PORT_VALUE
+            echo "[Env_var] $REQUESTED_PORT_NAME variable set to $REQUESTED_PORT_VALUE"
         fi
 
-        echo "[Env_var] $REQUESTED_PORT_NAME variable set to $REQUESTED_PORT_VALUE"
+        if [[ -z ${!PRIVATE_REQUESTED_PORT_NAME} ]]; then
+            echo "[Env_var] Variable $PRIVATE_REQUESTED_PORT_NAME does not exist. Exporting ..."
+            export $PRIVATE_REQUESTED_PORT_NAME=$PRV_REQUESTED_PORT_VALUE
+            echo "[Env_var] $PRIVATE_REQUESTED_PORT_NAME variable set to $PRV_REQUESTED_PORT_VALUE"
+        fi
     fi
 fi
