af9f0d000f18be5a82770bd7d6dff09fb0c0cdbc
alijawadfahs <alijawadfahs@gmail.com>
2023-06-26 23:25:10 +0200

    making the get ip address more redundant (#49)

--- sal-service/src/main/resources/get_ip_addr_script.groovy
+++ sal-service/src/main/resources/get_ip_addr_script.groovy
@@ -1,7 +1,40 @@
-URL url_name = new URL("https://api.ipify.org");
-BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
-// reads system IPAddress
-systemipaddress = sc.readLine().trim();
-System.out.println("Public IP Address: " + systemipaddress + "\n");
+import groovy.transform.Synchronized
 
-result = systemipaddress
+// Define a function to retrieve the IP from a server
+def retrieveIPFromServer(server) {
+    try {
+        def url = "http://${server}"
+        def response = new URL(url).text
+        return response.trim()
+    } catch (Exception e) {
+        println "Failed to retrieve IP from ${server}: ${e.message}"
+        return null
+    }
+}
+
+// Define a function to check the IP from multiple servers
+@Synchronized
+def checkIPFromServers() {
+    def servers = [
+            "checkip.amazonaws.com",
+            "api.ipify.org",
+            "ifconfig.me",
+            "ipinfo.io/ip",
+            "icanhazip.com",
+            "ident.me",
+            "myip.dnsomatic.com"
+    ];
+    for (def server : servers) {
+        def ip = retrieveIPFromServer(server)
+        if (ip) {
+            println "Address was retrieved from ${server}"
+            println "Public IP: ${ip}"
+            return ip
+        }
+    }
+
+    println "Unable to retrieve the public IP from any server."
+}
+
+// Call the function to check the IP
+result = checkIPFromServers()
