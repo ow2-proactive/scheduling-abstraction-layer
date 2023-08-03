2e2b49293b81e26b1c911d404671c02bb070777c
alijawadfahs <alijawadfahs@gmail.com>
2023-08-03 10:15:23 +0200

    modified the remove cloud endpoint to make it faster (#60)
    * modified the remove cloud endpoint to make it faster
    
    * Khalil's comments

--- sal-service/src/main/java/org/ow2/proactive/sal/service/repository/HardwareRepository.java
+++ sal-service/src/main/java/org/ow2/proactive/sal/service/repository/HardwareRepository.java
@@ -29,6 +29,7 @@ import java.util.List;
 
 import org.ow2.proactive.sal.model.Hardware;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,4 +41,9 @@ public interface HardwareRepository extends JpaRepository<Hardware, String> {
     @Transactional(readOnly = true)
     @Query(value = "SELECT id FROM Hardware WHERE id NOT IN (SELECT hardware.id FROM NodeCandidate GROUP BY hardware.id)")
     List<String> getOrphanHardwareIds();
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "DELETE FROM Hardware WHERE id NOT IN (SELECT hardware.id FROM NodeCandidate GROUP BY hardware.id)")
+    void deleteOrphanHardwareIds();
+
 }
