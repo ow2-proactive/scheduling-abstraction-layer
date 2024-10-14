0ef0d7e2f0e51386f210fa3ed11b038abf465f3f
ankicabarisic <ankica.barisic@activeeon.com>
2024-10-14 15:02:38 +0200

    update the github scripts to be pointed as raw

--- docker/scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
+++ docker/scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
@@ -2,7 +2,7 @@
 echo "Master pre-install script\n"
 
 sudo hostnamectl set-hostname "$variables_PA_JOB_NAME"
-sudo -H -u ubuntu bash -c 'wget https://github.com/ow2-proactive/scheduling-abstraction-layer/tree/master/docker/bootstrap-scripts/nm-bootstrap-script.sh && chmod +x nm-bootstrap-script.sh'
+sudo -H -u ubuntu bash -c 'wget https://raw.githubusercontent.com/ow2-proactive/scheduling-abstraction-layer/master/docker/bootstrap-scripts/nm-bootstrap-script.sh && chmod +x nm-bootstrap-script.sh'
 sudo -H -u ubuntu bash -c "./nm-bootstrap-script.sh 'CREATE' 'MASTER' $APP_UUID $ONM_IP";
 echo ""
 echo ""
--- docker/scripts/WORKER_INSTALL_SCRIPT.sh
+++ docker/scripts/WORKER_INSTALL_SCRIPT.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Worker install script"
-wget https://github.com/ow2-proactive/scheduling-abstraction-layer/tree/master/docker/bootstrap-scripts/install-kube-u22.sh && chmod +x ./install-kube-u22.sh && ./install-kube-u22.sh
+wget https://raw.githubusercontent.com/ow2-proactive/scheduling-abstraction-layer/master/docker/bootstrap-scripts/install-kube-u22.sh && chmod +x ./install-kube-u22.sh && ./install-kube-u22.sh
